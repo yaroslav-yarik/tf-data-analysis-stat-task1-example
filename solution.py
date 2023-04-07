499c8f31db07c30e3043685a0d295ee08d34ddf1
yaroslav-yarik <86802498+yaroslav-yarik@users.noreply.github.com>
2023-04-07 13:22:39 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -2,10 +2,11 @@ import pandas as pd
 import numpy as np
 
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 561049466 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array) -> float:
     # Измените код этой функции
     # Это будет вашим решением
     # Не меняйте название функции и её аргументы
-    return x.mean() # Ваш ответ
+    
+    return np.average(x) / 84 # Ваш ответ
